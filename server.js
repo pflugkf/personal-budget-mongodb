531cf57c9d4e4d8da10f49f71826c988b43f90f9
Kristin Pflug <pflugkf@gmail.com>
2024-03-03 16:17:34 -0500

    completed part 2 of assignment 08

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Budget API
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
@@ -10,10 +13,10 @@ app.use(cors());
 const mongoose = require("mongoose");
 const budgetModel = require("./models/pb_schema");
 
-let url = 'mongodb://localhost:27017/assignment_08';
+let url = "mongodb://localhost:27017/assignment_08";
 
 let chartData = {
-    myBudget: []
+  myBudget: [],
 };
 
 /* const budget = {
@@ -33,38 +36,66 @@ let chartData = {
     ]
 }; */
 
-//app.use('/', express.static('public'));
+app.use("/", express.static("public"));
+var jsonParser = bodyParser.json();
 
 /* app.get('/hello', (req, res) => {
     res.send('Hello World!');
 }); */
 
-mongoose.connect(url).then(() => {
+mongoose
+  .connect(url)
+  .then(() => {
     console.log("Connected to database");
 
-    budgetModel.find().then((data) => {
+    budgetModel
+      .find()
+      .then((data) => {
         console.log(data);
         chartData.myBudget = data;
         mongoose.connection.close();
-    }).catch((connectionError) => {
+      })
+      .catch((connectionError) => {
         console.log(connectionError);
-    });
-
-}).catch((connectionError) => {
+      });
+  })
+  .catch((connectionError) => {
     console.log(connectionError);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/budget", (req, res) => {
+  //const budget = require('./budget-data.json');
+
+  //res.json(budget);
+  res.send(chartData);
 });
 
-app.get('/budget', (req, res) => {
-    //const budget = require('./budget-data.json');
+app.post("/newdoc", jsonParser, (req, res) => {
+  console.log(req.body);
 
-    //res.json(budget);
-    res.send(chartData);
+  mongoose.connect(url).then(() => {
+      console.log("Connected to database");
+
+      let newData = new budgetModel(req.body);
+      budgetModel.insertMany(newData).then((data) => {
+          console.log(data);
+          mongoose.connection.close();
+          res.status(200).send("New document added: " + data);
+        })
+        .catch((connectionError) => {
+          console.log(connectionError);
+          res.status(400).send(connectionError);
+        });
+    })
+    .catch((connectionError) => {
+      console.log(connectionError);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`API served at http://localhost:${port}`);
 });
